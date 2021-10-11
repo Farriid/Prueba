dd32183ce56540d066177e2e290f569dd8f2e490
Farriid <farid270606>
2021-10-11 12:42:57 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+let one = document.getElementById("one");
+let pay = document.getElementById("pay");
+pay.addEventListener("click", () =>{
+  one.play();
+})
+
+let dos = document.getElementById("dos");
+let pay1 = document.getElementById("pay1");
+pay1.addEventListener("click", () =>{
+  dos.play();
+})
+
+let tres = document.getElementById("tres");
+let pay2 = document.getElementById("pay2");
+pay2.addEventListener("click", () =>{
+  tres.play();
+})
+
+let cuatro = document.getElementById("cuatro");
+let pay3 = document.getElementById("pay3");
+pay3.addEventListener("click", () =>{
+  cuatro.play();
+})
+
+let cinco = document.getElementById("cinco");
+let pay4 = document.getElementById("pay4");
+pay4.addEventListener("click", () =>{
+  cinco.play();
+})
+
+let seis = document.getElementById("seis");
+let pay5 = document.getElementById("pay5");
+pay5.addEventListener("click", () =>{
+  seis.play();
+})
+
+let siete = document.getElementById("siete");
+let pay6 = document.getElementById("pay6");
+pay6.addEventListener("click", () =>{
+  siete.play();
+})
+
+let ocho= document.getElementById("ocho");
+let pay7 = document.getElementById("pay7");
+pay7.addEventListener("click", () =>{
+ocho.play();
+})
+
+let nueve = document.getElementById("nueve");
+let pay8 = document.getElementById("pay8");
+pay8.addEventListener("click", () =>{
+  nueve.play();
+})
+
+
